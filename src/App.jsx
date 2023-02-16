182deaa57800df2170414aac6f4764c096fd1b1f
Marcos <padilladortamarcosalejandro@gmail.com>
2023-02-16 12:41:34 -0500

    Added design of the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,69 @@
 export default function App() {
+	const temporalTodos = [
+		{
+			id: 1,
+			todo: 'Go shopping',
+			description: 'Buy some food for the dinner',
+			completed: false,
+		},
+		{
+			id: 2,
+			todo: 'Study programming',
+			description: 'Continue studying react and vue',
+			completed: true,
+		},
+	]
+
 	return (
 		<div>
-			<h1>Hola Mundo desde div</h1>
+			<div className='list'>
+				<ul
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						gap: 10,
+						listStyle: 'none',
+						width: '60%',
+					}}
+				>
+					{temporalTodos.map((todo) => {
+						return (
+							<li
+								key={todo.id}
+								style={{
+									display: 'flex',
+									justifyContent: 'space-between',
+									placeItems: 'center',
+									backgroundColor: todo.completed
+										? 'rgb(34,117,76)'
+										: 'rgb(50,50,50)',
+									padding: 20,
+									borderRadius: 10,
+								}}
+							>
+								<div style={{ display: 'flex', flexDirection: 'column' }}>
+									<h3 style={{ marginBottom: 0, fontWeight: 'bold' }}>
+										{todo.todo}
+									</h3>
+									<span style={{ color: 'darkgray' }}>{todo.description}</span>
+								</div>
+								<div
+									style={{
+										display: 'flex',
+										gap: '10px',
+									}}
+								>
+									<button>Completar</button>
+									<button>Eliminar</button>
+								</div>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+			<div className='additem'>
+				<form></form>
+			</div>
 		</div>
 	)
 }
